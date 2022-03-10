57a16713eda524488fb33d3cbf646b6d338843ae
Matheus Lenke <matheuslenke@gmail.com>
2022-03-10 08:48:26 -0300

    Desafio palíndromo done

---
+++ Playgrounds/Aula_03/Palindromo.playground/Contents.swift
@@ -0,0 +1,35 @@
+import UIKit
+
+// <--- Palíndromos --->
+// reler, rever, reviver, rir, socos, radar
+
+func checkPalindromo(word: String) -> Bool {
+    var mirroredWord = ""
+    var wordArray: [String.Element] = []
+
+    // Criando um array com cada caracter
+    word.forEach({ char in
+        wordArray.append(char)
+    })
+    
+    // Criando a string espelhada
+    let wordSize = wordArray.count
+    for i in 0...wordSize - 1 {
+        mirroredWord.append(wordArray[wordSize - 1 - i])
+    }
+
+    if (mirroredWord == word) {
+        print("A palavra \(word) é um palíndromo!")
+        return true
+    }
+    print("A palavra \(word) não é um palíndromo!")
+    return false
+}
+
+checkPalindromo(word: "testando")
+checkPalindromo(word: "ovo")
+checkPalindromo(word: "reler")
+checkPalindromo(word: "rever")
+checkPalindromo(word: "rir")
+checkPalindromo(word: "godev")
+checkPalindromo(word: "idwall")
